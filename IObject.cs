175a5354d6d2e0ceebba6ae690a099acaef91303
Oksird <m.muz.al3105@gmail.com>
2022-06-27 20:54:09 +0300

    Added Types: Point and Vector. Added Interface

---
+++ IObject.cs
@@ -0,0 +1,13 @@
+﻿using TrueComputerGraphics.Types;
+
+namespace TrueComputerGraphics.Interfaces
+{
+    public interface IObject
+    {
+        public bool IsInterceptionWithRay(Point start, Vector direction);
+
+        public Point WhereInterceptionWithRay(Point start, Vector direction);
+
+        public Vector GetNormalOnPoint(Point point);
+    }
+}
