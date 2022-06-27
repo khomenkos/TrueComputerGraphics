9ccdbc505f83f7ae3e5173ea7abbb15825c1245d
khomenkos <khomenkos196@gmail.com>
2022-06-27 20:35:52 +0300

    init

---
+++ TrueComputerGraphics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrueComputerGraphics
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
