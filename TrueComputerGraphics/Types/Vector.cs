f55ac6fa72ea5bdc390283e6ef61f0a928642957
Oksird <m.muz.al3105@gmail.com>
2022-06-27 20:58:00 +0300

    Fix project structure

---
+++ TrueComputerGraphics/Types/Vector.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace TrueComputerGraphics.Types
+{
+    public class Vector
+    {
+        public float x { get; }
+        public float y { get; }
+        public float z { get; }
+
+        public Vector(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public static Vector GetVectorWithPoints(Point p1, Point p2)
+        {
+            return new Vector(p2.x - p1.x, p2.y - p1.y, p2.z - p1.z);
+        }
+
+        public float Magnitude()
+        {
+            return (float)Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2) + Math.Pow(z, 2));
+        }
+
+        public static Vector Normilize(Vector vector)
+        {
+            var mag = vector.Magnitude();
+            if (mag > 0)
+            {
+                return new Vector(vector.x / mag, vector.y / mag, vector.z / mag);
+            }
+            return vector;
+        }
+
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            return new Vector(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+        }
+
+        public static Vector operator *(float v1, Vector v2)
+        {
+            return new Vector(v1 * v2.x, v1 * v2.y, v1 * v2.z);
+        }
+
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            return new Vector(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+        }
+
+        public static float operator *(Vector v1, Vector v2)
+        {
+            return (float)(v1.x * v2.x + v1.y * v2.y + v1.z * v2.z);
+        }
+
+        public static Vector Cross(Vector u, Vector v)
+        {
+            return new Vector(
+                u.y * v.z - u.z * v.y,
+                u.z * v.x - u.x * v.z,
+                u.x * v.y - u.y * v.x);
+        }
+
+        public static Vector Negate(Vector u)
+        {
+            return new Vector(-u.x, -u.y, -u.z);
+        }
+    }
+}
